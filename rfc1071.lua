aad556752ebd5be601a327b4da8235658f507546
Connor Horman <chorman64@gmail.com>
2019-08-11 19:34:52 -0400

    Add a hopefully well done rfc1071 implementation.

---
+++ rfc1071.lua
@@ -0,0 +1,29 @@
+---
+--Pure Lua Implementation of RFC 1071.
+local rfc1071 = {};
+
+local int2bin = require"apis.int2bin";
+
+local function add(a,b)
+  local result = a+b;
+  local rem = result%65536;
+  local carry = (result-rem)/65536;
+  if carry==0 then
+    return rem
+  else
+    return add(rem,carry);
+  end
+end
+
+function rfc1071.checksum(message)
+  local sum = 0;
+  local messageLen = (#message)/2;
+  for a=0,messageLen do
+    local val = int2bin.tohword(message:sub(2*messageLen+1,2*(messageLen+1)));
+    sum = add(sum,val);
+  end
+  return int2bin.hword(sum);
+end
+
+
+return rfc1071;
